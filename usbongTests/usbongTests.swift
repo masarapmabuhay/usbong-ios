a8333f1bfaef3c6b49166d1d11adeaefb2e92754
Chris Amanse <christopheramanse@gmail.com>
2015-09-15 11:39:27 +0800

    Initial Commit

---
+++ usbongTests/usbongTests.swift
@@ -0,0 +1,36 @@
+//
+//  usbongTests.swift
+//  usbongTests
+//
+//  Created by Joe Amanse on 15/09/2015.
+//  Copyright © 2015 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+import XCTest
+@testable import usbong
+
+class usbongTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
