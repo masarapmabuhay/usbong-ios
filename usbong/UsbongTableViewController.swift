3fcca53fa9081003f29c3876c95fd480c8e36de4
Chris Amanse <christopheramanse@gmail.com>
2015-09-15 12:50:18 +0800

    Added test cells for UsbongTableViewController

--- usbong/UsbongTableViewController.swift
+++ usbong/UsbongTableViewController.swift
@@ -28,24 +28,20 @@ class UsbongTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
-
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = "Usbong \(indexPath.row)"
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
