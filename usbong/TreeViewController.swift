d655238be5d43e8004215a15262816b8842127bd
Chris Amanse <christopheramanse@gmail.com>
2015-09-15 14:54:42 +0800

    Replace page view controller to view controller

---
+++ usbong/TreeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TreeViewController.swift
+//  usbong
+//
+//  Created by Chris Amanse on 9/15/15.
+//  Copyright © 2015 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+import UIKit
+
+// Root view controller for Tree (Page-based)
+class TreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
