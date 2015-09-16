c30e5d1431438c7fcf049aa481ed2437044bd330
Chris Amanse <christopheramanse@gmail.com>
2015-09-16 23:42:40 +0800

    Added TODO. Used TextDisplay for demo.

--- usbong/TreeViewController.swift
+++ usbong/TreeViewController.swift
@@ -19,10 +19,11 @@ class TreeViewController: UIViewController {
         super.viewDidLoad()
 
         // Test TextDisplayViewController
-//        let viewController = TextDisplayNodeViewController(nibName: "TextDisplayNodeViewController", bundle: NSBundle.mainBundle())
+        // TODO: Use table view controllers instead. For modularity of components such as text, image, etc.
+        let viewController = TextDisplayNodeViewController(nibName: "TextDisplayNodeViewController", bundle: NSBundle.mainBundle())
         
         // Test TextDisplayViewController
-        let viewController = ImageDisplayNodeViewController(nibName: "ImageDisplayNodeViewController", bundle: NSBundle.mainBundle())
+//        let viewController = ImageDisplayNodeViewController(nibName: "ImageDisplayNodeViewController", bundle: NSBundle.mainBundle())
         
         addChildViewController(viewController)
         containerView.addSubview(viewController.view)
