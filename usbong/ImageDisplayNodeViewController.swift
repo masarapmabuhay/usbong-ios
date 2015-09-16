fa459216e1365a7c101f973baa1515c8e347ac88
Chris Amanse <christopheramanse@gmail.com>
2015-09-16 23:03:01 +0800

    Removed hacky method. Set image to have a fixed aspect ratio.

--- usbong/ImageDisplayNodeViewController.swift
+++ usbong/ImageDisplayNodeViewController.swift
@@ -12,18 +12,19 @@ class ImageDisplayNodeViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print(imageView.image?.size)
-        print(UIScreen.mainScreen().bounds.size)
-        
-        imageView.hidden = true
+    }
+    
+    // Hacky method - has noticeable delay
+//    override func viewDidAppear(animated: Bool) {
+//        print(imageView.image?.size)
+//        print(imageView.frame.size)
+//        
 //        // Compute scale
 //        if let imageWidth = imageView.image?.size.width {
-//            let scale = imageWidth / UIScreen.mainScreen().bounds.width
+//            let scale = imageWidth / imageView.frame.width
 //            print(scale)
 //            if scale > 1 {
 //                print("Image has larger width")
@@ -33,27 +34,7 @@ class ImageDisplayNodeViewController: UIViewController {
 //                imageViewHeightConstraint.constant *= scale
 //            }
 //        }
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        print(imageView.image?.size)
-        print(imageView.frame.size)
-        
-        // Compute scale
-        if let imageWidth = imageView.image?.size.width {
-            let scale = imageWidth / imageView.frame.width
-            print(scale)
-            if scale > 1 {
-                print("Image has larger width")
-                imageViewHeightConstraint.constant /= scale
-            } else {
-                print("Image has smaller width")
-                imageViewHeightConstraint.constant *= scale
-            }
-        }
-        
-        imageView.hidden = false
-    }
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
