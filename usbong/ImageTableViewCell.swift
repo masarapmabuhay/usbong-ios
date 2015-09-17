f1f071e7d71ffaaf482437b0843843114ea1b166
Chris Amanse <christopheramanse@gmail.com>
2015-09-17 12:43:38 +0800

    Added ImageTableViewCell module

---
+++ usbong/ImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageTableViewCell.swift
+//  usbong
+//
+//  Created by Joe Amanse on 17/09/2015.
+//  Copyright © 2015 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
