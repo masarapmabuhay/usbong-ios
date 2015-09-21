0c3a6a4c6a4c2a7c51367e0a47bf545015ce7062
Chris Amanse <christopheramanse@gmail.com>
2015-09-21 22:24:53 +0800

    Prepared unpacking functions

--- usbong/UsbongFileManager.swift
+++ usbong/UsbongFileManager.swift
@@ -19,6 +19,7 @@ class UsbongFileManager {
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1]
     }()
+    var temporaryDirectoryURL = NSURL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true).URLByAppendingPathComponent("trees", isDirectory: true)
     
     func contentsOfDirectoryAtRootURL() -> [NSURL]? {
         return try? NSFileManager.defaultManager().contentsOfDirectoryAtURL(rootURL, includingPropertiesForKeys: nil, options: NSDirectoryEnumerationOptions.SkipsHiddenFiles)
@@ -34,4 +35,39 @@ class UsbongFileManager {
     }
     
     var defaultFileName = "Unnamed"
+    
+    func unpackTreeWithURL(url: NSURL, toDestinationURL destinationURL: NSURL) -> Bool {
+        // Make sure tree URL is a file and destination url is a directory, else, return false
+        guard url.fileURL && !destinationURL.fileURL else {
+            return false
+        }
+        
+        // Unpack code here
+        
+        return true
+    }
+    
+    func unpackTreeToTemporaryDirectoryWithTreeURL(treeURL: NSURL) -> Bool {
+        // Make sure treeURL is a file, else, return false
+        guard treeURL.fileURL else {
+            return false
+        }
+        
+        print("TemporaryDirectoryURL: \(temporaryDirectoryURL)")
+        
+        // If tempDirectoryURL does not exist or is not a directory, create directory
+        var isDirectory: ObjCBool = true
+        let fileManager = NSFileManager.defaultManager()
+        if !fileManager.fileExistsAtPath(temporaryDirectoryURL.path ?? "", isDirectory: &isDirectory) || !isDirectory {
+            print("Temp directory does not exist")
+            do {
+                try fileManager.createDirectoryAtURL(temporaryDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+                print("Created temporary directory")
+            } catch {
+                return false
+            }
+        }
+        
+        return unpackTreeWithURL(treeURL, toDestinationURL: temporaryDirectoryURL)
+    }
 }
