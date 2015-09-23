c0816514830dcecd0d8d3a42c804586f744a0a30
Chris Amanse <christopheramanse@gmail.com>
2015-09-24 07:31:11 +0800

    Used UsbongTree

--- usbong/TreeViewController.swift
+++ usbong/TreeViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class TreeViewController: UIViewController {
     
     var pageViewController: UIPageViewController?
-    var taskNodes: [TaskNode] = []
+//    var taskNodes: [TaskNode] = []
     var treeZipURL: NSURL?
     var tree: UsbongTree?
     
@@ -23,17 +23,27 @@ class TreeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Unpack tree (place this on a background thread if noticeable lag occurs)
+        if let zipURL = treeZipURL {
+            if let url = UsbongFileManager.defaultManager().unpackTreeToTemporaryDirectoryWithTreeURL(zipURL) {
+                tree = UsbongTree(treeDirectoryURL: url)
+                
+                // Set navigation bar title to tree name
+                navigationItem.title = tree?.name
+            }
+        }
+        
         // Task Nodes
         // Test task nodes
-        let textDisplay = TextDisplayTaskNode(text: "Some Text.")
-        let imageDisplay = ImageDisplayTaskNode(imageFilePath: "someFile")
-        let textImageDisplay = TextImageDisplayTaskNode(text: "Some Text.", imageFilePath: "someFilePath")
-        let imageTextDisplay = ImageTextDisplayTaskNode(imageFilePath: "someFilePath", text: "Some Text.")
-        
-        taskNodes.append(textDisplay)
-        taskNodes.append(imageDisplay)
-        taskNodes.append(textImageDisplay)
-        taskNodes.append(imageTextDisplay)
+//        let textDisplay = TextDisplayTaskNode(text: "Some Text.")
+//        let imageDisplay = ImageDisplayTaskNode(imageFilePath: "someFile")
+//        let textImageDisplay = TextImageDisplayTaskNode(text: "Some Text.", imageFilePath: "someFilePath")
+//        let imageTextDisplay = ImageTextDisplayTaskNode(imageFilePath: "someFilePath", text: "Some Text.")
+//        
+//        taskNodes.append(textDisplay)
+//        taskNodes.append(imageDisplay)
+//        taskNodes.append(textImageDisplay)
+//        taskNodes.append(imageTextDisplay)
         
         // Page view controller
         pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
@@ -52,16 +62,6 @@ class TreeViewController: UIViewController {
             pageVC.view.frame = containerView.bounds
             pageVC.didMoveToParentViewController(self)
         }
-        
-        // Unpack tree (place this on a background thread if noticeable lag occurs)
-        if let zipURL = treeZipURL {
-            if let url = UsbongFileManager.defaultManager().unpackTreeToTemporaryDirectoryWithTreeURL(zipURL) {
-                tree = UsbongTree(treeDirectoryURL: url)
-                
-                // Set navigation bar title to tree name
-                navigationItem.title = tree?.name
-            }
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,13 +72,16 @@ class TreeViewController: UIViewController {
     // MARK: - Custom
     
     func viewControllerAtIndex(index: Int) -> TaskNodeTableViewController? {
-        guard taskNodes.count != 0 && index < taskNodes.count else {
+        guard tree?.taskNodes.count != 0 && index < tree?.taskNodes.count else {
             return nil
         }
         
         // Assign task node for view controller
         let taskVC = TaskNodeTableViewController()
-        taskVC.taskNode = taskNodes[index]
+        if let taskNode = tree?.taskNodes[index] {
+            print("TaskNode found: \(taskNode)")
+            taskVC.taskNode = taskNode
+        }
         
         return taskVC
     }
